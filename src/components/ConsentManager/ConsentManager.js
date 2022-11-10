77bea9f614341a2ad54cf670a30e9a5d2322bce0
rizkyridwan <rizky.ridwan@trustedshops.de>
2022-11-10 17:06:10 +0100

    Install Axios

---
+++ src/components/ConsentManager/ConsentManager.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class ConsentManager extends React.Component {
+    render () {
+        return (
+            <div>
+                <div>Präsenzen absichern</div>
+                <div>ConsentManager</div>
+            </div>
+        );
+    }
+};
+
+export default ConsentManager
---
+++ src/lib/ApiAuth.js
@@ -0,0 +1,9 @@
+class ApiAuth
+{
+    constructor(username, password)
+    {
+        this.username = username;
+        this.password = password;
+    }
+
+};
